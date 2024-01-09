1ca149fd3249a30272121aa20e0d964710851090
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2024-01-09 23:25:51 +0900

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.20"
-    id("io.ktor.plugin") version "2.2.1"
+    id("io.ktor.plugin") version "2.2.4"
 }
 
 group = "me.yapoo"
@@ -17,17 +17,20 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.2.2")
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.2.2")
-    implementation("io.ktor:ktor-server-netty-jvm:2.2.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.2.2")
-    implementation("io.ktor:ktor-serialization-jackson:2.2.2")
-    implementation("io.ktor:ktor-server-status-pages-jvm:2.2.2")
-    implementation("io.insert-koin:koin-ktor:3.3.0")
-    implementation("io.insert-koin:koin-logger-slf4j:3.3.0")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-jackson")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+
+    implementation("io.insert-koin:koin-ktor:3.5.3")
+    implementation("io.insert-koin:koin-logger-slf4j:3.5.3")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("com.webauthn4j:webauthn4j-core:0.20.7.RELEASE")
     implementation("ch.qos.logback:logback-classic:1.4.12")
-    implementation("io.ktor:ktor-server-cors-jvm:2.2.2")
     implementation("com.upokecenter:cbor:4.5.3")
 }
